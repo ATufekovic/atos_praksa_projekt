47c205c9976a3cfa121d56ce93dbbdfcdd796288
ATufekovic <anto.tufekovic@gmail.com>
2020-10-20 15:15:35 +0200

    added extra functionality to register.html

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -6,12 +6,14 @@ angular.module("chatApp").controller("registerCtrl", function($scope, $http) {
     
 
     $scope.register = function() {
-        $scope.users = [];
+        /*$scope.users = [];
         $scope.userForm = {
             username : $scope.username,
             password : $scope.password,
             email : $scope.email
-        };
+        };*/
+        if(usernameIsValid($scope.username) && passwordIsValid($scope.password) && emailIsValid($scope.email)) $scope.username="dsa";
+        
     }
     $scope.submitUser = function() {
          
@@ -61,8 +63,25 @@ angular.module("chatApp").controller("registerCtrl", function($scope, $http) {
             else {
               $scope.error = false;
             }
+            if(usernameIsValid($scope.username)) $scope.usernameWrongInput=false;
+            else $scope.usernameWrongInput=true;
+            if(passwordIsValid($scope.password)) $scope.passwordWrongInput=false;
+            else $scope.passwordWrongInput=true;
+            if(emailIsValid($scope.email)) $scope.emailWrongInput=false;
+            else $scope.emailWrongInput=true;
         }
 
+        function usernameIsValid(username) {
+            var testUsername=username;
+            return /^(?=.{8,50}$)[a-zA-Z]([_.-]?[a-zA-Z0-9])*$/.test(username);
+        }
 
+        function passwordIsValid(password){
+            return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,128}$/.test(password);
+        }
+
+        function emailIsValid(email){
+            return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+        }
     
 });
